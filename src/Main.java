9c233f9138ded6ba7c32a960207b66de0d9af7e9
h.panahi.ird <h.panahi.ird@gmail.com>
2019-07-02 01:23:58 +0430

    read input file line by line

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,18 @@
-import java.io.File;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
         try {
             File file = new File("input.txt");
-            if (file.isFile())
-                System.out.println(file.length());
-        }catch (Exception e){
+            if (file.isFile()) {
+                BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                String line;
+                while ((line = bufferedReader.readLine()) != null){
+                    System.out.println(line);
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
