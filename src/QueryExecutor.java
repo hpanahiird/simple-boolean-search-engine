e8617c9d3d6c78ff1a24ad0431c317d4f6f26df8
h.panahi.ird <h.panahi.ird@gmail.com>
2019-07-05 00:33:19 +0430

    split query by white space and parenthesis.

--- src/QueryExecutor.java
+++ src/QueryExecutor.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 class QueryExecutor {
     private InvertedIndex invertedIndex;
 
@@ -6,6 +10,19 @@ class QueryExecutor {
     }
 
     void execute(String query){
+        parseQuery(query);
         System.out.println(query);
+    }
+
+    private void parseQuery(String query){
+        List<String> splitted = new ArrayList<>();
+        splitted.addAll(Arrays.asList(query.split("[\\s]+")));
+        for (int i = splitted.size()-1;i>=0;i--){
+            List list = Arrays.asList(splitted.get(i).split("((?<=\\()|(?=\\())|((?<=\\))|(?=\\)))"));
+            splitted.remove(i);
+            splitted.addAll(i,list);
+        }
+        System.out.println(splitted);
+//        System.out.println(Arrays.toString(query.split("((?<=\\()|(?=\\())|( )|((?<=\\))|(?=\\)))")));
     }
 }
