06918e1b8cf613d84ec9a111d0b19118e4d9b740
h.panahi.ird <h.panahi.ird@gmail.com>
2019-07-04 22:48:50 +0430

    get query and exit if query is the exit command.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.Scanner;
 
 public class Main {
 
@@ -13,6 +14,17 @@ public class Main {
                     invertedIndex.indexDocument(line);
                 }
                 invertedIndex.print();
+            }
+            System.out.println("Welcome to Simple Boolean Search Engine!");
+            System.out.println("please enter your query and hit enter to start search.");
+            System.out.println("to exit just type \"exit!!\" and hit enter.");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Query: ");
+            String query = scanner.nextLine();
+            while (!"exit!!".equals(query)){
+                System.out.println(query);
+                System.out.print("Query: ");
+                query = scanner.nextLine();
             }
         } catch (Exception e) {
             e.printStackTrace();
